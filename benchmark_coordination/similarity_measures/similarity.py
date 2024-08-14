36dd390597a906947447d6b800d537336559a21d
letizia iannucci <letizia.iannucci@aalto.fi>
2024-08-14 15:28:23 +0300

    move example to docstring

--- benchmark_coordination/similarity_measures/similarity.py
+++ benchmark_coordination/similarity_measures/similarity.py
@@ -7,6 +7,14 @@ from benchmark_coordination.types.similarity import Sim
 class SimilarityCalculator:
     """
     A class to calculate similarity between two vectors using different similarity measures.
+    ----------------
+    Example:
+    ----------------
+    >>> similarity_calculator = SimilarityCalculator("cosine")
+    >>> vector1 = [1, 2, 3]
+    >>> vector2 = [4, 5, 6]
+    >>> similarity_calculator.calculate_similarity(vector1, vector2)
+    0.9746318461970762
     """
 
     def __init__(self, similarity_score: Sim) -> None:
@@ -14,7 +22,7 @@ class SimilarityCalculator:
         Initialize the SimilarityCalculator object with the similarity score to be used.
         :param similarity_score: str, the similarity score to be used.
             If the similarity score is not one of the following: "cosine", "jaccard",
-            a ValueError will be raised.
+            "ratclif-obershelp", a ValueError will be raised.
         :return: None
         """
         self.similarity_score = similarity_score
@@ -22,6 +30,7 @@ class SimilarityCalculator:
         self.similarity_measures: Dict[str, Callable] = {
             "cosine": scores.cosine_similarity,
             "jaccard": scores.jaccard_similarity,
+            "ratclif-obershelp": scores.ratclif_obershelp_similarity,
         }
 
         if self.similarity_score not in self.similarity_measures:
@@ -38,11 +47,3 @@ class SimilarityCalculator:
         return self.similarity_measures[self.similarity_score](
             vector1, vector2, **kwargs
         )
-
-
-# Example usage
-similarity_calculator = SimilarityCalculator("cosine")
-vector1 = [1, 2, 3]
-vector2 = [4, 5, 6]
-similarity = similarity_calculator.calculate_similarity(vector1, vector2)
-print(similarity)
