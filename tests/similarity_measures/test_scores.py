86b87e23fe1ceb9767919b579235701ea47ef315
letizia iannucci <letizia.iannucci@aalto.fi>
2024-08-15 20:55:56 +0300

    parametrize

--- tests/similarity_measures/test_scores.py
+++ tests/similarity_measures/test_scores.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import pytest
 from benchmark_coordination.similarity_measures.scores import (
     cosine_similarity,
     jaccard_similarity,
@@ -10,202 +10,128 @@ from benchmark_coordination.similarity_measures.scores import (
 )
 
 
-def test_cosine_similarity():
+@pytest.mark.parametrize(
+    "vector1, vector2, expected_result",
+    [
+        (np.array([1, 2, 3]), np.array([4, 5, 6]), 0.9746318461970762),
+        (np.array([1, 2, 3]), np.array([1, 2, 3]), 1.0),
+        (np.array([1, 2, 3]), np.array([-1, -2, -3]), -1.0),
+    ],
+)
+def test_cosine_similarity(vector1, vector2, expected_result):
     """
     Test the cosine_similarity function.
     """
-    vector1 = np.array([1, 2, 3])
-    vector2 = np.array([4, 5, 6])
     result = cosine_similarity(vector1, vector2)
-    expected_result = 0.9746318461970762
-    assert np.isclose(
-        result, expected_result
-    ), f"Expected cosine_similarity {expected_result}, got {result}"
-
-    vector1 = np.array([1, 2, 3])
-    vector2 = np.array([1, 2, 3])
-    result = cosine_similarity(vector1, vector2)
-    expected_result = 1.0
-    assert np.isclose(
-        result, expected_result
-    ), f"Expected cosine_similarity {expected_result}, got {result}"
-
-    vector1 = np.array([1, 2, 3])
-    vector2 = np.array([-1, -2, -3])
-    result = cosine_similarity(vector1, vector2)
-    expected_result = -1.0
     assert np.isclose(
         result, expected_result
     ), f"Expected cosine_similarity {expected_result}, got {result}"
 
 
-def test_jaccard_similarity_text():
+@pytest.mark.parametrize(
+    "vector1, vector2, expected_result",
+    [
+        (
+            np.array(["a", "b", "c", "d", "e"]),
+            np.array(["a", "b", "c", "d", "f"]),
+            0.6666666,
+        ),
+        (np.array(["a", "b", "c", "d", "e"]), np.array(["a", "b", "c", "d", "e"]), 1.0),
+        (np.array(["a", "b", "c", "d", "e"]), np.array(["f", "g", "h", "i", "j"]), 0.0),
+    ],
+)
+def test_jaccard_similarity_text(vector1, vector2, expected_result):
     """
     Test the jaccard_similarity function on text.
     """
-    vector1 = np.array(["a", "b", "c", "d", "e"])
-    vector2 = np.array(["a", "b", "c", "d", "f"])
     result = jaccard_similarity(vector1, vector2)
-    expected_result = 0.6666666
-    assert np.isclose(
-        result, expected_result
-    ), f"Expected jaccard_similarity {expected_result}, got {result}"
-
-    vector1 = np.array(["a", "b", "c", "d", "e"])
-    vector2 = np.array(["a", "b", "c", "d", "e"])
-    result = jaccard_similarity(vector1, vector2)
-    expected_result = 1.0
-    assert np.isclose(
-        result, expected_result
-    ), f"Expected jaccard_similarity {expected_result}, got {result}"
-
-    vector1 = np.array(["a", "b", "c", "d", "e"])
-    vector2 = np.array(["f", "g", "h", "i", "j"])
-    result = jaccard_similarity(vector1, vector2)
-    expected_result = 0.0
     assert np.isclose(
         result, expected_result
     ), f"Expected jaccard_similarity {expected_result}, got {result}"
 
 
-def test_jaccard_similarity():
+@pytest.mark.parametrize(
+    "vector1, vector2, expected_result",
+    [
+        (np.array([1, 2, 3, 4, 5]), np.array([2, 3, 4, 5, 6]), 0.6666666),
+        (np.array([1, 0, 0]), np.array([0, 0, 1]), 1.0),
+        (np.array([1, 0, 0]), np.array([1, 1, 1]), 0.5),
+        (np.array([1, 1, 1, 1, 1]), np.array([0, 0, 0, 0, 0]), 0.0),
+    ],
+)
+def test_jaccard_similarity(vector1, vector2, expected_result):
     """
     Test the jaccard_similarity function.
     """
-    vector1 = np.array([1, 2, 3, 4, 5])
-    vector2 = np.array([2, 3, 4, 5, 6])
     result = jaccard_similarity(vector1, vector2)
-    expected_result = 0.6666666
-    assert np.isclose(
-        result, expected_result
-    ), f"Expected jaccard_similarity {expected_result}, got {result}"
-
-    vector1 = np.array([1, 0, 0])
-    vector2 = np.array([0, 0, 1])
-    result = jaccard_similarity(vector1, vector2)
-    expected_result = 1.0
-    assert np.isclose(
-        result, expected_result
-    ), f"Expected jaccard_similarity {expected_result}, got {result}"
-
-    vector1 = np.array([1, 0, 0])
-    vector2 = np.array([1, 1, 1])
-    result = jaccard_similarity(vector1, vector2)
-    expected_result = 0.5
-    assert np.isclose(
-        result, expected_result
-    ), f"Expected jaccard_similarity {expected_result}, got {result}"
-
-    vector1 = np.array([1, 1, 1, 1, 1])
-    vector2 = np.array([0, 0, 0, 0, 0])
-    result = jaccard_similarity(vector1, vector2)
-    expected_result = 0.0
     assert np.isclose(
         result, expected_result
     ), f"Expected jaccard_similarity {expected_result}, got {result}"
 
 
-def test_jaccard_binary_similarity():
+@pytest.mark.parametrize(
+    "vector1, vector2, expected_result",
+    [
+        (np.array([1, 1, 0, 0, 1]), np.array([1, 0, 1, 0, 1]), 0.5),
+        (np.array([1, 0, 1, 0, 1]), np.array([1, 0, 1, 0, 1]), 1.0),
+        (np.array([1, 0, 1, 0, 1]), np.array([0, 0, 0, 0, 0]), 0.0),
+    ],
+)
+def test_jaccard_binary_similarity(vector1, vector2, expected_result):
     """
     Test the jaccard_binary_similarity function.
     """
-    vector1 = np.array([1, 1, 0, 0, 1])
-    vector2 = np.array([1, 0, 1, 0, 1])
     result = jaccard_binary_similarity(vector1, vector2)
-    expected_result = 0.5
-    assert np.isclose(
-        result, expected_result
-    ), f"Expected jaccard_binary_similarity {expected_result}, got {result}"
-
-    vector1 = np.array([1, 0, 1, 0, 1])
-    vector2 = np.array([1, 0, 1, 0, 1])
-    result = jaccard_binary_similarity(vector1, vector2)
-    expected_result = 1.0
-    assert np.isclose(
-        result, expected_result
-    ), f"Expected jaccard_binary_similarity {expected_result}, got {result}"
-
-    vector1 = np.array([1, 0, 1, 0, 1])
-    vector2 = np.array([0, 0, 0, 0, 0])
-    result = jaccard_binary_similarity(vector1, vector2)
-    expected_result = 0.0
     assert np.isclose(
         result, expected_result
     ), f"Expected jaccard_binary_similarity {expected_result}, got {result}"
 
 
-def test__longest_common_substring():
+@pytest.mark.parametrize(
+    "s1, s2, expected_result",
+    [
+        ("abcdfgh", "abedfhr", "ab"),
+        ("abcdfgh", "abcdfgh", "abcdfgh"),
+        ("abcdfgh", "fghijkl", "fgh"),
+    ],
+)
+def test__longest_common_substring(s1, s2, expected_result):
     """
     Test the _longest_common_substring function.
     """
-    s1 = "abcdfgh"
-    s2 = "abedfhr"
     result = _longest_common_substring(s1, s2)
-    expected_result = "ab"
-    assert result == expected_result, f"Expected {expected_result}, got {result}"
-
-    s1 = "abcdfgh"
-    s2 = "abcdfgh"
-    result = _longest_common_substring(s1, s2)
-    expected_result = "abcdfgh"
-    assert result == expected_result, f"Expected {expected_result}, got {result}"
-
-    s1 = "abcdfgh"
-    s2 = "fghijkl"
-    result = _longest_common_substring(s1, s2)
-    expected_result = "fgh"
     assert result == expected_result, f"Expected {expected_result}, got {result}"
 
 
-def test__matching_characters():
+@pytest.mark.parametrize(
+    "s1, s2, expected_result",
+    [
+        ("abcdfgh", "abedfhr", 5),
+        ("abcdfgh", "abcdfgh", 7),
+    ],
+)
+def test__matching_characters(s1, s2, expected_result):
     """
     Test the _matching_characters function.
     """
-    s1 = "abcdfgh"
-    s2 = "abedfhr"
     result = _matching_characters(s1, s2)
-    expected_result = 5
-    assert result == expected_result, f"Expected {expected_result}, got {result}"
-
-    s1 = "abcdfgh"
-    s2 = "abcdfgh"
-    result = _matching_characters(s1, s2)
-    expected_result = 7
     assert result == expected_result, f"Expected {expected_result}, got {result}"
 
 
-def test_ratcliff_obershelp_similarity():
+@pytest.mark.parametrize(
+    "vector1, vector2, expected_result",
+    [
+        (np.array([1, 2, 3, 4, 5]), np.array([1, 2, 3, 4, 6]), 0.8),
+        (np.array([1, 2, 3, 4, 5]), np.array([2, 3, 4, 5, 6]), 0.8),
+        (np.array([1, 2, 3, 4, 5]), np.array([0, 0, 0, 0, 0]), 0.0),
+        (np.array([1, 2, 3, 4, 5]), np.array([1, 2, 3, 4, 5]), 1.0),
+    ],
+)
+def test_ratcliff_obershelp_similarity(vector1, vector2, expected_result):
     """
     Test the ratcliff_obershelp_similarity function.
     """
-    vector1 = np.array([1, 2, 3, 4, 5])
-    vector2 = np.array([1, 2, 3, 4, 6])
     result = ratcliff_obershelp_similarity(vector1, vector2)
-    expected_result = 0.8
-    assert np.isclose(
-        result, expected_result
-    ), f"Expected ratcliff_obershelp_similarity {expected_result}, got {result}"
-
-    vector1 = np.array([1, 2, 3, 4, 5])
-    vector2 = np.array([2, 3, 4, 5, 6])
-    result = ratcliff_obershelp_similarity(vector1, vector2)
-    expected_result = 0.8
-    assert np.isclose(
-        result, expected_result
-    ), f"Expected ratcliff_obershelp_similarity {expected_result}, got {result}"
-
-    vector1 = np.array([1, 2, 3, 4, 5])
-    vector2 = np.array([0, 0, 0, 0, 0])
-    result = ratcliff_obershelp_similarity(vector1, vector2)
-    expected_result = 0.0
-    assert np.isclose(
-        result, expected_result
-    ), f"Expected ratcliff_obershelp_similarity {expected_result}, got {result}"
-
-    vector1 = np.array([1, 2, 3, 4, 5])
-    vector2 = np.array([1, 2, 3, 4, 5])
-    result = ratcliff_obershelp_similarity(vector1, vector2)
-    expected_result = 1.0
     assert np.isclose(
         result, expected_result
     ), f"Expected ratcliff_obershelp_similarity {expected_result}, got {result}"
