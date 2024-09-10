f3ae002f6a0de8af04e7eed3ab95c18d4e1a08c6
letizia iannucci <letizia.iannucci@aalto.fi>
2024-09-10 21:26:19 +0300

    refactor

---
+++ benchmark_coordination/similarity_calculator/similarity.py
@@ -0,0 +1,54 @@
+from typing import Callable, Dict, List
+
+import benchmark_coordination.similarity_calculator.scores as scores
+from benchmark_coordination.types.similarity_types import SimilarityMeasure
+
+
+class SimilarityCalculator:
+    """
+    A class to calculate similarity between two vectors using different similarity measures.
+    ----------------
+    Example:
+    ----------------
+    >>> similarity_calculator = SimilarityCalculator("cosine")
+    >>> vector1 = [1, 2, 3]
+    >>> vector2 = [4, 5, 6]
+    >>> similarity_calculator.calculate_similarity(vector1, vector2)
+    0.9746318461970762
+    """
+
+    def __init__(self, similarity_score: SimilarityMeasure) -> None:
+        """
+        Initialize the SimilarityCalculator object with the similarity score to be used.
+        :param similarity_score: str, the similarity score to be used.
+            If the similarity score is not one of the following:
+                "cardinality"
+                "cosine"
+                "jaccard"
+                "ratcliff-obershelp"
+            a ValueError will be raised.
+        :return: None
+        """
+        self.similarity_score = similarity_score
+
+        self.similarity_measures: Dict[str, Callable] = {
+            "cardinality": scores.cardinality_similarity,
+            "cosine": scores.cosine_similarity,
+            "jaccard": scores.jaccard_similarity,
+            "ratcliff-obershelp": scores.ratcliff_obershelp_similarity,
+        }
+
+        if self.similarity_score not in self.similarity_measures:
+            raise ValueError("Invalid similarity score")
+
+    def calculate_similarity(self, vector1: List, vector2: List, **kwargs) -> float:
+        """
+        Calculate the similarity between two vectors using the specified similarity score.
+        :param vector1: list, the first vector.
+        :param vector2: list, the second vector.
+        :param kwargs: dict, additional arguments to be passed to the similarity measure function.
+        :return: float, the similarity score between the two vectors.
+        """
+        return self.similarity_measures[self.similarity_score](
+            vector1, vector2, **kwargs
+        )
