3345475f4a79c41757ba32dd39ba969b261f420b
letizia iannucci <letizia.iannucci@aalto.fi>
2024-09-12 22:21:28 +0300

    add projection

--- benchmark_coordination/network_builder/bipartite.py
+++ benchmark_coordination/network_builder/bipartite.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import pandas as pd
+from typing import Optional
 
 
 def build_bipartite_graph(
@@ -45,3 +46,63 @@ def build_bipartite_graph(
             source, target, weight=row["count"], timestamps=row.get("timestamp", [])
         )
     return G
+
+
+def project_on_nodes(
+    bipartite: nx.Graph,
+    nodes: list,
+    weight: Optional[str] = None,
+) -> nx.Graph:
+    """
+    Project a bipartite graph on a set of nodes.
+    :param bipartite: nx.Graph, the input bipartite graph.
+    :param nodes: list, the list of nodes to project on.
+    :param weight: str, the attribute to use as weight for the edges in the projected graph.
+        Default is None, in which case the weight is the sum of the edges to the common neighbors.
+    :return: nx.Graph, the projected graph.
+    ----------------
+    Example:
+    ----------------
+    >>> import networkx as nx
+    >>> B = nx.Graph()
+    >>> B.add_nodes_from(["1", "2", "3", "4", "5", "6"], bipartite=0)
+    >>> B.add_nodes_from(["A", "B", "C", "D"], bipartite=1)
+    >>> B.add_edges_from(
+    ...     [
+    ...         ("1", "A", {"weight": 1}),
+    ...         ("1", "B", {"weight": 1}),
+    ...         ("2", "B", {"weight": 1}),
+    ...         ("2", "C", {"weight": 1}),
+    ...         ("3", "C", {"weight": 1}),
+    ...         ("3", "D", {"weight": 1}),
+    ...     ]
+    ... )
+    >>> nodes = ["1", "2", "3"]
+    >>> project_on_nodes(B, nodes).edges(data=True)
+    EdgeDataView([('1', '2', {'weight': 1}), ('1', '3', {'weight': 1}), ('2', '3', {'weight': 1})])
+    >>> project_on_nodes(B, nodes, weight="weight").edges(data=True)
+    EdgeDataView([('1', '2', {'weight': 2}), ('1', '3', {'weight': 2}), ('2', '3', {'weight': 2})])
+    """
+    P = nx.Graph()
+    P.add_nodes_from(nodes)
+    # for each node, find the neighbors in the bipartite graph
+    for u in nodes:
+        for v in nodes:
+            if u == v:
+                continue
+            # common neighbors
+            neighbors = set(bipartite[u]) & set(bipartite[v])
+
+            # weight is the number of common neighbors
+            # or the sum of the weights of the edges to the common neighbors
+            w = (
+                sum(
+                    bipartite[u][n][weight] + bipartite[v][n][weight] for n in neighbors
+                )
+                if weight is not None
+                else len(neighbors)
+            )
+            if w > 0:
+                P.add_edge(u, v, weight=w)
+
+    return P
