20620292d0b0dba2ab9c1448fd0e128b9b084310
letizia iannucci <letizia.iannucci@aalto.fi>
2024-09-12 22:21:37 +0300

    tests

--- tests/test_network_builder_bipartite.py
+++ tests/test_network_builder_bipartite.py
@@ -1,6 +1,10 @@
+import pytest
 import pandas as pd
 import networkx as nx
-from benchmark_coordination.network_builder.bipartite import build_bipartite_graph
+from benchmark_coordination.network_builder.bipartite import (
+    build_bipartite_graph,
+    project_on_nodes,
+)
 
 
 def test_build_bipartite_graph_notime():
@@ -63,6 +67,56 @@ def test_build_bipartite_graph_withtime():
         result, nx.Graph
     ), f"Expected a NetworkX graph, got {type(result)}"
 
+    assert (
+        list(result.edges(data=True)) == expected_edges
+    ), f"Expected {expected_edges}, got {result.edges(data=True)}"
+
+
+@pytest.fixture
+def bipartite_graph():
+    B = nx.Graph()
+    B.add_nodes_from(["1", "2", "3", "4", "5", "6"], bipartite=0)
+    B.add_nodes_from(["A", "B", "C", "D"], bipartite=1)
+    B.add_edges_from(
+        [
+            ("1", "A", {"weight": 1}),
+            ("1", "B", {"weight": 2}),
+            ("2", "B", {"weight": 1}),
+            ("2", "C", {"weight": 1}),
+            ("3", "C", {"weight": 3}),
+            ("3", "D", {"weight": 1}),
+        ]
+    )
+    return B
+
+
+def test_project_on_nodes_unweighted(bipartite_graph):
+    """
+    Test the project_on_nodes function without weights.
+    """
+    nodes = ["1", "2", "3"]
+    result = project_on_nodes(bipartite=bipartite_graph, nodes=nodes)
+    expected_edges = [("1", "2", {"weight": 1}), ("2", "3", {"weight": 1})]
+
+    assert isinstance(
+        result, nx.Graph
+    ), f"Expected a NetworkX graph, got {type(result)}"
+    assert (
+        list(result.edges(data=True)) == expected_edges
+    ), f"Expected {expected_edges}, got {result.edges(data=True)}"
+
+
+def test_project_on_nodes_weighted(bipartite_graph):
+    """
+    Test the project_on_nodes function with weights.
+    """
+    nodes = ["1", "2", "3"]
+    result = project_on_nodes(bipartite=bipartite_graph, nodes=nodes, weight="weight")
+    expected_edges = [("1", "2", {"weight": 3}), ("2", "3", {"weight": 4})]
+
+    assert isinstance(
+        result, nx.Graph
+    ), f"Expected a NetworkX graph, got {type(result)}"
     assert (
         list(result.edges(data=True)) == expected_edges
     ), f"Expected {expected_edges}, got {result.edges(data=True)}"
